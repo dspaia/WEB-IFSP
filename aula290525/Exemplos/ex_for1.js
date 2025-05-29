ef1fad1295fb4e509f552705d161dc62c262a121
dspaia <campos.paiva@aluno.ifsp.edu.br>
2025-05-29 10:52:42 -0300

    Delete aula290525/Exemplos directory

--- aula290525/Exemplos/ex_for1.js
+++
@@ -1,9 +0,0 @@
-//Exemplo 1 - Imprimindo quantidade de elefantes
-
-for(let i = 1; i < 20; i += 2){
-    if (i === 7){
-        break
-    }
-
-    console.log("Número total de elefantes: " + i)
-}
--- aula290525/Exemplos/ex_sw4.js
+++
@@ -1,8 +0,0 @@
-//Exemplo 4 - Bom dia
-
-contador = 0
-
-while(contador < 3){
-    contador += 1
-    console.log("Bom Dia!")
-}
